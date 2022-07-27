a5531e7050b9a877f50cf83a54c01604f4f62790
Denzel Witbooi <dwitbooi28@gmail.com>
2022-07-27 09:30:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/jsonapi/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.jsonapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String url = "https://www.7timer.info/bin/astro.php?lon=-33.96&lat=25.62&product=civil&unit=metric&output=json&tzshift=0";
+    private RequestQueue requestQueue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("JSON:", "onResponse: " + response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error", "onErrorResponse: " + error);
+                    }
+                });
+        requestQueue.add(jsonObjectRequest);
+    }
+}
